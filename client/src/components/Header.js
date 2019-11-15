7274db195dcd2808687cd53182869fed5d02a3c0
Mohammad Jaiteh <jaitehmohammad.com>
2019-11-14 19:20:03 -0500

    carosel

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -26,14 +26,26 @@ class Header extends Component {
                 <div class="carousel-inner">
                     <div class="carousel-item active">
                         <img class="d-block w-100" src="https://cdn.pixabay.com/photo/2017/02/12/16/46/shoes-2060519_960_720.jpg" alt="First slide" />
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>v5</h5>
+                            <p>Coming soon..</p>
+                        </div>
                     </div>
 
                     <div class="carousel-item">
                         <img class="d-block w-100" src="https://cdn.pixabay.com/photo/2017/02/12/16/46/shoes-2060519_960_720.jpg" alt="Second slide" />
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>d3</h5>
+                            <p>Stay tune..</p>
+                        </div>
                     </div>
 
                     <div class="carousel-item">
                         <img class="d-block w-100" src="https://cdn.pixabay.com/photo/2017/02/12/16/46/shoes-2060519_960_720.jpg" alt="Third slide" />
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Smoothy smooth</h5>
+                            <p>Coming out in 2 weeks ...</p>
+                        </div>
                     </div>
 
                 </div>
